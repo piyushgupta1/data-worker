208d7c5ff5f65dcd45b852c4ce195f7413ae9388
Piyush Gupta <Piyush2-Gupta>
2018-01-06 13:08:23 +0530

    feat($core): Sorting with grouping
    Now you can group and sort at the same time

--- src/data-worker.ts
+++ src/data-worker.ts
@@ -1,55 +1,103 @@
+import {
+  ascending,
+  descending,
+  deviation,
+  max,
+  mean,
+  median,
+  min,
+  quantile,
+  sum,
+  variance
+} from 'd3-array'
 import { nest } from 'd3-collection'
 
-import { AggregatorMode } from './enum'
-import { IAggregatorArrayModeInput, IAggregatorMapModeInput } from './interface'
-import { mean, nanMean, sum } from './operators'
+import { AggregatorMode, GroupingMode, Operation, SortingMode } from './enum'
+import {
+  IAggregatorArrayModeInput,
+  IAggregatorMapModeInput,
+  IAttrSortingMode,
+  IGroupOnlyMode,
+  IGroupSortingMode
+} from './interface'
 
 export class DataWorker {
-  private ipData: any[] = []
-  private result: any[] = []
-  private groupApplied: boolean = false
-  private nestingApplied: boolean = false
+  private _ipData: any[] = []
+  private _result: any[] = []
+  private _groupApplied: boolean = false
+  private _nestingApplied: boolean = false
 
   constructor() {
     //
   }
 
-  public group(groupOn: string[], flat: boolean = false): DataWorker {
-    if (this.ipData.length === 0) {
-      throw EvalError('Empty data to group not allowed')
+  /**
+   * Should group elements also apply sorting based on parameter given
+   * Input Options described below
+   * IGroupOnlyMode = {
+   *  mode<GroupingMode> -> Grouping Mode used
+   *  flat<boolean> -> Flatten the resultant structure
+   *  groupOn<string[]> -> Group on these attributes while maintaining order
+   * }
+   *
+   * IGroupSortingMode = {
+   *  mode<GroupingMode> -> Grouping Mode used
+   *  flat<boolean> -> Flatten the resultant structure
+   *  groupingOptions<{ attribute: string,sortMode: SortingMode}[]> -> Array of Json with
+   *    special sorting order on every grouping attribute
+   * }
+   * @param options All grouping options
+   * @returns this
+   */
+  public group(options: IGroupOnlyMode | IGroupSortingMode): DataWorker {
+    // Check Data Input
+    if (this._ipData.length === 0) {
+      throw new EvalError('Empty data to group not allowed')
     }
 
-    if (groupOn.length === 0) {
-      throw EvalError('Empty parameter to group not allowed')
-    }
+    // Check Mode for grouping
+    const mode = options.mode
+    if (mode === GroupingMode.ONLY_GROUP) {
+      options = options as IGroupOnlyMode
 
-    let tempResult = []
-    const nestObject = nest()
-    for (const key of groupOn) {
-      nestObject.key((d: any) => {
-        return d[key]
-      })
-    }
-    tempResult = nestObject.entries(this.ipData)
-    if (flat) {
-      this.result = this.flattenObjects(tempResult, groupOn)
+      // Validations in case Group only mode is used
+      if (options.groupOn.length === 0) {
+        throw EvalError('Empty parameter to group not allowed')
+      }
+
+      this._result = this._putGroupingOnInput(
+        options.flat,
+        options.groupOn.map(item => {
+          return { sortMode: SortingMode.NONE, attribute: item }
+        }),
+        this._ipData
+      )
+    } else if (mode === GroupingMode.GROUP_SORT) {
+      options = options as IGroupSortingMode
+
+      // Validations in case Group-Sort mode is used
+      if (options.groupingOptions.length === 0) {
+        throw EvalError('Empty parameter to group not allowed')
+      }
+      this._result = this._putGroupingOnInput(options.flat, options.groupingOptions, this._ipData)
     } else {
-      this.nestingApplied = true
-      this.result = tempResult
+      // Throw error if invalid option is used
+      throw EvalError('InValid Grouping Mode')
     }
-    this.groupApplied = true
+    this._nestingApplied = !options.flat
+    this._groupApplied = true
     return this
   }
 
   public aggregate(
     aggregatorOptions: IAggregatorArrayModeInput | IAggregatorMapModeInput
   ): DataWorker {
-    if (this.ipData.length === 0) {
+    if (this._ipData.length === 0) {
       throw TypeError('No Data Present')
     }
 
     const mode = aggregatorOptions.mode
-    let finalAttrToAggrMap = new Map<string, string[]>()
+    let finalAttrToAggrMap = new Map<string, Operation[]>()
     if (mode === AggregatorMode.ARRAY_MODE) {
       aggregatorOptions = aggregatorOptions as IAggregatorArrayModeInput
       if (aggregatorOptions.attrs.length === 0) {
@@ -67,38 +115,62 @@ export class DataWorker {
       finalAttrToAggrMap = aggregatorOptions.attrToAggrMap
     }
 
-    if (this.groupApplied) {
-      if (this.nestingApplied) {
-        this.putAggregationOnNestedResult(this.result, finalAttrToAggrMap)
+    if (this._groupApplied) {
+      if (this._nestingApplied) {
+        this._putAggregationOnNestedResult(this._result, finalAttrToAggrMap)
       } else {
-        for (const singleElement of this.result) {
+        for (const singleElement of this._result) {
           Object.assign(
             singleElement,
-            this.createTempAggregationObject(finalAttrToAggrMap, singleElement.values)
+            this._createTempAggregationObject(finalAttrToAggrMap, singleElement.values)
           )
           delete singleElement.values
         }
       }
     } else {
-      this.result.push(this.createTempAggregationObject(finalAttrToAggrMap, this.ipData))
+      this._result.push(this._createTempAggregationObject(finalAttrToAggrMap, this._ipData))
     }
 
     return this
   }
 
   public input(entryData: any[]): DataWorker {
-    this.ipData = Object.assign([], entryData)
+    this._ipData = Object.assign([], entryData)
     return this
   }
 
   public complete(): any[] {
-    this.reset()
-    return this.result
+    this._reset()
+    return this._result
   }
 
-  private putAggregationOnNestedResult(
+  private _putGroupingOnInput(flat: boolean, groupOnMapping: IAttrSortingMode[], input: any[]) {
+    let tempResult = []
+    const nestObject = nest()
+    const groupOn: string[] = []
+    groupOnMapping.forEach(oneKey => {
+      groupOn.push(oneKey.attribute)
+      nestObject.key((d: any) => {
+        return d[oneKey.attribute]
+      })
+      if (oneKey.sortMode === SortingMode.ASC) {
+        nestObject.sortKeys(ascending)
+      } else if (oneKey.sortMode === SortingMode.DSC) {
+        nestObject.sortKeys(descending)
+      }
+    })
+
+    tempResult = nestObject.entries(input)
+    if (flat) {
+      return this._flattenObjects(tempResult, groupOn)
+    } else {
+      return tempResult
+    }
+  }
+
+  private _putAggregationOnNestedResult(
     inputData: any[],
-    finalAttrToAggrMap: Map<string, string[]>
+    finalAttrToAggrMap: Map<string, Operation[]>
   ) {
     for (const singleElement of inputData) {
       if (singleElement.hasOwnProperty('key') && singleElement.hasOwnProperty('values')) {
@@ -106,58 +178,90 @@ export class DataWorker {
           if (!singleElement.values[0].hasOwnProperty('values')) {
             Object.assign(
               singleElement,
-              this.createTempAggregationObject(finalAttrToAggrMap, singleElement.values)
+              this._createTempAggregationObject(finalAttrToAggrMap, singleElement.values)
             )
             delete singleElement.values
           } else {
-            this.putAggregationOnNestedResult(singleElement.values, finalAttrToAggrMap)
+            this._putAggregationOnNestedResult(singleElement.values, finalAttrToAggrMap)
           }
         }
       }
     }
   }
 
-  private createTempAggregationObject(finalAttrToAggrMap: Map<string, string[]>, inputData: any[]) {
+  private _createTempAggregationObject(
+    finalAttrToAggrMap: Map<string, Operation[]>,
+    inputData: any[]
+  ) {
     const tempObject: any = {}
     finalAttrToAggrMap.forEach((aggr, attr) => {
       aggr.forEach(singleAgg => {
-        tempObject[singleAgg + '_' + attr] = this.oneAttrOneAggr(inputData, attr, singleAgg)
+        tempObject[Operation[singleAgg] + '_' + attr] = this._oneAttrOneAggr(
+          inputData,
+          attr,
+          singleAgg
+        )
       })
     })
     return tempObject
   }
 
-  private oneAttrOneAggr(input: any[], attr: string, aggr: string) {
-    let functionToPut
+  private _oneAttrOneAggr(input: any[], attr: string, aggr: Operation): number {
+    input = input.map(item => item[attr])
+    let result: number | undefined
     switch (aggr) {
-      case 'nanmean':
-        functionToPut = nanMean
+      case Operation.NANMEAN:
+        result = mean(input)
         break
-      case 'mean':
-        functionToPut = mean
+      case Operation.MEAN:
+        result = mean(input)
         break
-      case 'average':
-        functionToPut = mean
+      case Operation.AVERAGE:
+        result = mean(input)
         break
-      case 'sum':
-        functionToPut = sum
+      case Operation.SUM:
+        result = sum(input)
+        break
+      case Operation.MINIMUN:
+        result = min(input)
+        break
+      case Operation.MAXIMUM:
+        result = max(input)
+        break
+      case Operation.MEDIAN:
+        result = median(input)
+        break
+      case Operation.VARIANCE:
+        result = variance(input)
+        break
+      case Operation.Q3:
+        result = quantile(input, 0.75)
+        break
+      case Operation.Q1:
+        result = quantile(input, 0.25)
+        break
+      case Operation.STDEV:
+        result = deviation(input)
         break
 
       default:
         throw TypeError('Unsupported Operation')
     }
-    return +input
-      .map(item => item[attr])
-      .reduce(functionToPut)
-      .toFixed(2)
+
+    if (result === undefined) {
+      throw Error()
+    }
+
+    result = result as number
+    return result
   }
 
-  private flattenObjects(input: any[], keys: string[]): any[] {
+  private _flattenObjects(input: any[], keys: string[]): any[] {
     keys.push('values')
-    return this.parseOneArray(input, keys)
+    return this._parseOneArray(input, keys)
   }
 
-  private parseOneArray(
+  private _parseOneArray(
     input: any[],
     keys: string[],
     currentDepth: number = 0,
@@ -167,7 +271,7 @@ export class DataWorker {
     for (const singleElement of input) {
       if (singleElement.hasOwnProperty('key') && singleElement.hasOwnProperty('values')) {
         currentElement[keys[currentDepth]] = singleElement['key']
-        this.parseOneArray(
+        this._parseOneArray(
           singleElement['values'],
           keys,
           currentDepth + 1,
@@ -183,9 +287,9 @@ export class DataWorker {
     return returnArray
   }
 
-  private reset() {
-    this.ipData = []
-    this.nestingApplied = false
-    this.groupApplied = false
+  private _reset() {
+    this._ipData = []
+    this._nestingApplied = false
+    this._groupApplied = false
   }
 }
--- src/enum.ts
+++ src/enum.ts
@@ -2,3 +2,28 @@ export enum AggregatorMode {
   MAP_MODE = 1,
   ARRAY_MODE = 2
 }
+
+export enum GroupingMode {
+  ONLY_GROUP = 1,
+  GROUP_SORT = 2
+}
+
+export enum SortingMode {
+  ASC = 1,
+  DSC = 2,
+  NONE = 0
+}
+
+export enum Operation {
+  SUM = 0,
+  MEAN = 1,
+  AVERAGE = 2,
+  NANMEAN = 3,
+  MINIMUN = 4,
+  MAXIMUM = 5,
+  MEDIAN = 6,
+  VARIANCE = 7,
+  Q3 = 8,
+  Q1 = 9,
+  STDEV = 10
+}
